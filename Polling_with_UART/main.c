1139ca5cf963982ec34c7aa21f3d9edc20e67e66
Obed Oyandut <obed.oyandut@haw-hamburg.de>
2022-08-04 11:17:17 +0200

    Polling in UART v1

---
+++ Polling_with_UART/main.c
@@ -0,0 +1,105 @@
+   //======================================================================================================
+   // Author  : Obed Oyandut
+   // Date    : 04.08.2022
+   // Version : v1
+   //======================================================================================================
+   // This program uses TIVA TM4C1294XL Evaluation Board
+   //======================================================================================================
+   // It is little code to demonstrate polling in microcontroller and particularly in UART.
+   // Polling is not the best approach to read from FIFOs because it takes up useful processing
+   // time. Other efficient approach like interrupt can be used.  This code is only for demonstration
+   // purposes.
+   // The testing of this code is done with Realterm. The speed of the communication line is 115200MHz
+   // and a 8N1 format. The debugging is done in code composer studio.
+   //========================================================================================================
+   //                     ! IMPORTANT !
+   // This program runs endless. Stop with the "Red Square Button"
+   // in Debug Mode (Terminate = CTRL + F2)
+   //========================================================================================================
+
+#include "inc/tm4c1294ncpdt.h"
+#include <stdint.h>
+#include <stdio.h>
+
+    // Size of receive buffer
+
+#define LEN 32
+
+    // Buffer declarations
+
+unsigned char rxBuffer[LEN];
+unsigned char txBuffer[] = "Payload is received...send some more...";
+
+    //=========================================================================================
+    // Keeps count of the number of ASCII character received/sent
+    //=========================================================================================
+
+unsigned char count = 0;
+unsigned char i;
+
+    //=========================================================================================
+    // Configure UART2. First assign a clock to UART2. Wait for the clock
+    // to be assigned using a blocking while loop. Disable UART2 to perform configurations.
+    // Set the bit rate to 115200bps. Remember clock speed is 16MHz. Receiver speed is 16 times
+    // faster than transmit speed. Set the frame format to 8N1. Turn on all 16 FIFOs for receive
+    // transmit. Re enable UART2 after configuration.
+    //==========================================================================================
+
+void configUart2(void){
+    SYSCTL_RCGCUART_R |= (1<<2);
+    while((SYSCTL_PRUART_R & (1<<2))==0);
+    UART2_CTL_R &= ~(1<<0);
+    UART2_IBRD_R = 8;
+    UART2_FBRD_R = 44;
+    UART2_LCRH_R = 0x00000070;
+    UART2_CTL_R |= 0x301;
+}
+
+    //==========================================================================================
+    // Configure port D for UART. Assign clock to PD. Wait for clock to become stable using
+    //  a blocking while loop. Set alternate select of P(4) and PD(5). Set alternate function
+    // of PD(4) and PD(5)to UART.
+    //==========================================================================================
+
+void configPortD(void){
+    SYSCTL_RCGCGPIO_R |= (1<<3);
+    while((SYSCTL_PRGPIO_R & (1<<3)) == 0);
+    GPIO_PORTD_AHB_DEN_R |= 0x030;
+    GPIO_PORTD_AHB_AFSEL_R |= 0x030;
+    GPIO_PORTD_AHB_PCTL_R |= 0x110000;
+}
+
+void main(void){
+
+    //==========================================================================================
+    // Initialization UART2 and PD
+    //==========================================================================================
+
+    configUart2();
+    configPortD();
+
+    //======================================================================================================
+    // Polling is used in this case to check if the Rx FIFO has some content in it. If there is something,
+    // the processor copies the content into the receive up to 31 ASCII characters. Appends a string
+    // delimiter. Prints all the content of the rxBuffer as a string. If the content of the rxBuffer is
+    // not full, the the  processor continues to poll the Rx FIFOs. All 16 Rx FIFOs are used simultaneously.
+    // After printing the content of the rxBuffer, the processor sends a message to the peripheral "Payload
+    // is received...send some more...".
+    //=======================================================================================================
+
+    while(1)
+    {
+        while(UART2_FR_R & 0x10);
+        rxBuffer[count] = (char)(UART2_DR_R);
+        if (count == 31) {
+            rxBuffer[31] = '\0';
+            printf("Payload: %s\n",rxBuffer);
+            for (i=0;i<sizeof(txBuffer);i++) {
+                while(!(UART2_FR_R & 0x80));
+                UART2_DR_R = txBuffer[i];
+            }
+            count = 0;
+        }
+        count++;
+    }
+}
